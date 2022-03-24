15044e10d92721475b87a7eb902c0c5184368356
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-03-24 02:50:27 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,62 @@
 from ursina import *
 
-def Bullet():
-    bullet = Entity(model='cube',
-           color=color.blue,
-           texture='grass',
-           position=Turret.position,
-           rotation=(0, 0, 0),
-           scale=1,
-           collider='mesh',
-           )
+def bullet():
+    Bullet = Entity(model='cube',
+                    color=color.blue,
+                    texture='grass',
+                    position=TankChassis.position,
+                    rotation=(0, 0, 0),
+                    scale=1,
+                    collider='mesh',
+                    )
 
 def update():
     # Tank Chassis Movement-Forward/Backward
     if held_keys['w']:
-        TankChassis.position += TankChassis.forward * time.dt
+        global Speed
+        TankChassis.position += TankChassis.forward * time.dt * 10
     if held_keys['s']:
-        TankChassis.position -= TankChassis.forward * time.dt
+        TankChassis.position -= TankChassis.forward * time.dt * 10
     # Tank Chassis Movement-Rotate
     if held_keys['a']:
         TankChassis.rotation_y -= 50 * time.dt
     if held_keys['d']:
         TankChassis.rotation_y += 50 * time.dt
 
-    # Test Bullet
-    if held_keys['left mouse']:
-        Bullet()
-
     # Turret Movement
-    if held_keys['q']:
-        Turret.rotation_y -= 50 * time.dt
-    if held_keys['e']:
-        Turret.rotation_y += 50 * time.dt
+    if held_keys['q'] and camera.rotation_y > -20:
+        Turret.rotation_y -= 80 * time.dt
+        camera.rotation_y -= 30 * time.dt
+        camera.x += 5 * time.dt
+    if held_keys['e'] and camera.rotation_y < 20:
+        Turret.rotation_y += 80 * time.dt
+        camera.rotation_y += 30 * time.dt
+        camera.x -= 5 * time.dt
 
     # Test Enemy Movement
     TurretEn.rotation_y -= 50 * time.dt
     TankChassisEn.position += TankChassisEn.forward * time.dt
     TankChassisEn.rotation_y += 50 * time.dt
+
+    # Test Bullet
+    if held_keys['left mouse']:
+        bullet()
+
+    # Camera Zoom in and Out (Make Mouse Wheel if possible)
+    if held_keys['i'] and camera.rotation_x > 3:
+        camera.rotation_x -= 50 * time.dt
+        camera.z -= 50 * time.dt
+    if held_keys['k'] and camera.rotation_x < 25:
+        camera.rotation_x += 50 * time.dt
+        camera.z += 50 * time.dt
+
+    # Collision Testing
+    if TankChassis.intersects(TankChassisEn):
+        TankChassis.position -= TankChassis.forward * time.dt * 10
+    if TankChassis.intersects(BuildingBlock):
+        TankChassis.position -= TankChassis.forward * time.dt * 10
+    if TankChassis.intersects(BuildingBlock2):
+        TankChassis.position -= TankChassis.forward * time.dt * 10
 
 # Window Settings
 main = Ursina()
@@ -49,7 +70,7 @@ Ground = Entity(model='plane',
                 texture='grass',
                 position=(0, 0, 0),
                 rotation=(0, 0, 0),
-                scale=100,
+                scale=1000,
                 collider='mesh'
                 )
 
@@ -60,7 +81,7 @@ TankChassis = Entity(model='Assets/TankChassis.obj',
                      position=(0, 0, 0),
                      rotation=(0, 0, 0),
                      scale=1,
-                     collider='mesh',
+                     collider='box',
                      )
 
 # Spawn Turret on top and attached to TankChassis
@@ -75,33 +96,51 @@ Turret = Entity(parent=TankChassis,
 
 # Spawn Enemy Test TankChassis
 TankChassisEn = Entity(model='Assets/TankChassis.obj',
-                     color=color.blue,
-                     texture='grass',
-                     position=(20, 0, 0),
-                     rotation=(0, 0, 0),
-                     scale=1,
-                     collider='mesh',
-                     )
+                       color=color.gray,
+                       texture='Assets/Metal.jpg',
+                       position=(20, 0, 0),
+                       rotation=(0, 0, 0),
+                       scale=1,
+                       collider='mesh',
+                       )
 
 # Spawn Turret on top and attached to TankChassis
 TurretEn = Entity(parent=TankChassisEn,
-                model='Assets/Turret.obj',
-                color=color.blue,
-                texture='brick',
-                position=(0, 0, 0),
-                scale=1,
-                collider='mesh',
-                )
+                  model='Assets/Turret.obj',
+                  color=color.blue,
+                  texture='brick',
+                  position=(0, 0, 0),
+                  scale=1,
+                  collider='mesh',
+                  )
+
+# Buildings
+BuildingBlock = Entity(model='Assets/HouseBlock.obj',
+                       color=color.gray,
+                       texture='Assets/Metal.jpg',
+                       position=(30, 0, 200),
+                       rotation=(0, 0, 0),
+                       scale=0.8,
+                       collider='mesh',
+                       )
+
+BuildingBlock2 = Entity(model='Assets/HouseBlock.obj',
+                       texture='grass',
+                       position=(-10, 0, 200),
+                       rotation=(0, 180, 0),
+                       scale=0.8,
+                       collider='mesh',
+                       )
+
 
 
 # Make sky and editor camera
 Sky()
 camera.parent = TankChassis
-camera.x = 2
-camera.y = 10
-camera.rotation_x = 16
 
+# Camera Starting Position
+camera.z = -28
+camera.y = 10
+camera.rotation_x = 10
 # Run Program
 main.run()
-
-
